def766d624d0ed9a581f758a07e5da6ca4669d39
danielcunhas <danielcsantos258@gmail.com>
2022-09-10 14:26:56 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
+import copy from "copy-to-clipboard"
 import './App.css';
 
 function App() {
+  const [url, setUrl] = useState('')
+  const [urlFinal, setUrlFinal] = useState('')
+  const [hits, setHits] = useState('')
+  const [acessos, setAcessos] = useState('')
+  const [urlDados, setUrlDados] = useState('')
+  const [codigo, setCodigo] = useState('')
+  const [escondido, setEscondido] = useState(true)
+  const baseUrl = 'http://localhost:3000'
+  
+  
+  const copyToClipboard = () => {
+    copy(urlFinal);
+ }
+ const handleSubmit2 = async (event) => {
+  event.preventDefault()
+  const requestOptions = {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  };
+  const resultado = await fetch(`${baseUrl}/${codigo}/stats`, requestOptions)
+      .then(response => response.json())
+      .then(data => data);
+
+  console.log(resultado)
+  setHits(resultado.hits)
+  setAcessos(resultado.updatedAt)
+
+  setUrlDados(baseUrl + '/' + resultado.code)
+  setEscondido(false)
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ url: url })
+    };
+    const resultado = await fetch(`${baseUrl}/encurtador`, requestOptions)
+        .then(response => response.json())
+        .then(data => data);
+
+    console.log(resultado)
+    
+   
+    setUrlFinal(baseUrl + '/' + resultado.code)
+    
+  
+    }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" width="370" height="370" />
       </header>
+      <body className="App-body">
+        
+      
+        <form onSubmit={handleSubmit}>
+          <input 
+            placeholder='Insira a URL'
+            className="App-input"
+            type="text" 
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+        <input className="App-button" type="submit" />
+        </form>
+        <div className="copyTest">
+          <input type="text" value={urlFinal} className="App-input2" disabled="true" placeholder='Nova URL'/>      
+          <button onClick={copyToClipboard} className="App-button">Copiar link</button>
+        </div>
+        <div className="dadosUrl">
+          <h4>Dados de URL</h4>
+          <form onSubmit={handleSubmit2}>
+          <input 
+            placeholder='Insira o código'
+            className="App-input3"
+            type="text" 
+            value={codigo}
+            onChange={(e) => setCodigo(e.target.value)}
+          />
+          <input className="App-button" type="submit" value="Ver dados"/>
+        </form>
+          <table className='tabela' hidden={escondido}>
+            <tr>
+              <th>Url:</th>
+              <td>{urlDados}</td>
+            </tr>
+            <tr>
+              <th>Quantidade de Acessos:</th>
+              <td>{hits}</td>
+            </tr>
+            <tr>
+              <th>Último acesso:</th>
+              <td>{acessos}</td>
+            </tr>
+          </table>
+        </div>
+
+
+      </body>
     </div>
   );
 }
 
 export default App;
+
